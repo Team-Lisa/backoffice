7ead43417136f69b121d0ac1e745b1fb841d1fd3
matfonseca <mfonseca@fi.uba.ar>
2021-11-07 22:28:01 -0300

    refactor header app

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -4,13 +4,10 @@ import {Redirect} from "react-router-dom";
 const Home = () => {
 
     const logged_storage = localStorage.getItem('logged');
-    if(logged_storage === "true"){
-        return(
-            <Redirect  to="/content"/>
-        )
-    }else{
+    if(logged_storage !== "true"){
         return(<Redirect  to="/login"/>)
     }
+    return (<div>home</div>)
 }
 
 
