e297466b0fe30864c862ecbb4872d087e3407976
Gonzalo <gmarino@fi.uba.ar>
2021-12-07 22:23:21 -0300

    change style

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,83 +1,136 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {Redirect} from "react-router-dom";
 import ReactApexChart from "react-apexcharts";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { registerLocale, setDefaultLocale } from  "react-datepicker";
-import es from 'date-fns/locale/es';
+import {registerLocale, setDefaultLocale} from "react-datepicker";
+import {TextField} from "@material-ui/core";
+import {DesktopDatePicker, LocalizationProvider} from "@mui/lab";
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
 
 const Home = () => {
-    registerLocale('es', es);
-    setDefaultLocale('es');
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const color = "#203F58";
 
-    const logged_storage = localStorage.getItem('logged');
-    if(logged_storage !== "true"){
-        return(<Redirect  to="/login"/>)
-    }
+  const labels = ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'];
 
+  const logged_storage = localStorage.getItem('logged');
+  if (logged_storage !== "true") {
+    return (<Redirect to="/login"/>)
+  }
 
-
+  const filters = () => {
     return (
-    <div>
-        <h1>Dashboard</h1>
-        <div style={{backgroundColor: '#f2f9ff'}}>
-            <div>
-                <label>
-                    Desde:
-                    <DatePicker locale="es" dateFormat="dd/MM/yyyy" selected={startDate} onChange={(date) => setStartDate(date)} />
-                </label>
-                <label>
-                Hasta:
-                    <DatePicker locale="es" dateFormat="dd/MM/yyyy" selected={endDate} onChange={(date) => setEndDate(date)} />
-                </label>
-            </div>
-            <div>
-                <ReactApexChart options={{
-                chart: {
-                    height: 350,
-                    type: 'line',
-                },
-                colors: ['#CAA7F3', '#93D9F8', '#C4FEAC'],
-                stroke: {
-                    width: [0, 4]
-                },
-                title: {
-                    text: ''
-                },
-                dataLabels: {
-                    enabled: true,
-                    enabledOnSeries: [1]
-                },
-                labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'],
-                xaxis: {
-                    type: 'datetime'
-                },
-                yaxis: [{
-                    title: {
-                    text: 'Website Blog',
-                    },
-                
-                }, {
-                    opposite: true,
-                    title: {
-                    text: 'Social Media',
-                    }
-                }]
-                }} series={[{
-                name: 'Cantidad de unidades completas por los usuarios en un día',
-                type: 'column',
-                data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160]
-                }, {
-                name: 'Cantidad de accesos a la aplicación en el día',
-                type: 'line',
-                data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
-                }]} type="line" height={350} />
-            </div>
-        </div>
-    </div>
+      <div style={{display: 'flex', justifyContent: 'space-around'}}>
+        <label style={{display: 'flex', alignItems: 'center'}}>
+          <div style={{
+            fontFamily: 'Montserrat',
+            fontSize: 20,
+            color: "#203F58",
+            marginLeft: 20,
+            marginRight: 20,
+            fontWeight: 700
+          }}>
+            Desde:
+          </div>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DesktopDatePicker
+              inputFormat="dd/MM/yyyy"
+              value={startDate}
+              onChange={(date) => setStartDate(date)}
+              renderInput={(params) => <TextField {...params} sx={{
+                svg: {color},
+                input: {color},
+                label: {color}
+              }}/>}
+            />
+          </LocalizationProvider>
+        </label>
+        <label style={{display: 'flex', alignItems: 'center'}}>
+          <div style={{
+            fontFamily: 'Montserrat',
+            fontSize: 20,
+            color: "#203F58",
+            marginLeft: 20,
+            marginRight: 20,
+            fontWeight: 700
+          }}>
+            Hasta:
+          </div>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DesktopDatePicker
+              inputFormat="dd/MM/yyyy"
+              value={endDate}
+              onChange={(date) => setEndDate(date)}
+              renderInput={(params) => <TextField {...params} sx={{
+                svg: {color},
+                input: {color},
+                label: {color}
+              }}/>}
+            />
+          </LocalizationProvider>
+        </label>
+      </div>
     )
+  }
+
+
+  return (
+    <div style={{margin: 20}}>
+      <h1 style={{
+        fontFamily: 'Work Sans',
+        color: '#203F58',
+        fontSize: 42,
+        paddingTop: 10,
+        paddingBottom: 10
+      }}>Dashboard</h1>
+      <div style={{backgroundColor: 'rgb(240,240,240)', borderRadius: 20}}>
+        {filters()}
+        <div>
+          <ReactApexChart options={{
+            chart: {
+              height: 350,
+              type: 'line',
+              fontFamily: 'Montserrat',
+              foreColor: color,
+              toolbar: {
+                tools: {
+                  download: false,
+                  selection: true,
+                  zoom: false,
+                  zoomin: true,
+                  zoomout: true,
+                  pan: false,
+                  reset: true
+                },
+              },
+            },
+            dataLabel: {
+              fontFamily: 'Montserrat',
+              foreColor: color
+            },
+            colors: ['#CAA7F3', "#203F58", '#C4FEAC'],
+            dataLabels: {
+              enabled: true,
+              enabledOnSeries: [1]
+            },
+            labels: labels,
+            xaxis: {
+              type: 'datetime'
+            },
+          }} series={[{
+            name: 'Cantidad de unidades completas por los usuarios en un día',
+            type: 'column',
+            data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160]
+          }, {
+            name: 'Cantidad de accesos a la aplicación en el día',
+            type: 'line',
+            data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
+          }]} type="line" height={350}/>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home;
